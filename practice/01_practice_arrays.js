491ddaf51fda45ac4848af9334f46418b7303afa
Vasyl Maletych <v.maletych00@gmail.com>
2026-06-23 18:44:04 +0200

    Добавив теорію для метода reduce і рішив декілька завдань в практиці

--- practice/01_practice_arrays.js
+++ practice/01_practice_arrays.js
@@ -19,3 +19,15 @@ const processorsInStock = ["Ryzen 5 7600", "Core i5 13400F", "Ryzen 7 7800X3D", 
 const targetCpu = processorsInStock.find (cpu => cpu === "Ryzen 7 7800X3D");
 
 console.log(targetCpu);
+
+// задача 3 розрахунок ігрового часу за тиждень(reduce)
+
+const gamingHours = [2.5, 3, 1.5, 4, 2]
+
+const totalHours = gamingHours.reduce((acc, hours) => acc + hours, 0);
+
+// map
+
+const salaries = [15000, 22000, 18500, 30000];
+
+const salaryWithBonus = salaries.map (salary => salary + 500);
